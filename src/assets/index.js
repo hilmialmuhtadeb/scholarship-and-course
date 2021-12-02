95eb1384051bb790069870db84c610d647ce3b1c
Winda <niwayanwindayani@gmail.com>
2021-12-02 16:17:11 +0800

    adding icons and about us

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,9 @@
 import ScholarshipImage from "./images/scholarship-image.jpg";
 import HomepageImage from "./images/homepage.png";
+import IconWhatsapp from "./icons/whatsapp.png"
+import IconEmail from "./icons/email.png"
+import IconFacebook from "./icons/facebook.png"
+import IconInstagram from "./icons/instagram.png"
+import PhotoWinda from "./images/winda.jpg"
 
-export {ScholarshipImage, HomepageImage}
+export {ScholarshipImage, HomepageImage, IconWhatsapp, IconEmail, IconFacebook, IconInstagram, PhotoWinda}
