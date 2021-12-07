00ffdcbb5ece9ac8c3db5d85a08b885b5853f962
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2021-12-07 13:10:14 +0700

    activate pagination and handling add new scholarship

--- src/pages/AddScholarship/index.js
+++ src/pages/AddScholarship/index.js
@@ -1,12 +1,78 @@
-import React from 'react'
-import { ScholarshipForm } from '../../components'
+import React, { useState } from 'react'
+// import { ScholarshipForm } from '../../components'
+import { Button, Form, FormGroup, FormText, Input, Label } from 'reactstrap'
 import "./addScholarship.css"
 
 const AddScholarship = () => {
+  const [title, setTitle] = useState('');
+  const [deadline, setDeadline] = useState(null);
+  const [poster, setPoster] = useState('');
+  const [image, setImage] = useState(null);
+  const [description, setDescription] = useState('');
+
+  const onPosterUpload = (e) => {
+    const file = e.target.files[0];
+    setPoster(file);
+    setImage(URL.createObjectURL(file));
+  }
+
+  const onSubmit = () => {
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('deadline', deadline);
+    formData.append('poster', poster);
+    formData.append('description', description);
+
+    fetch('http://localhost:4000/v1/scholarship', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  
   return (
     <main className="container my-4">
       <h1>Tambah Informasi Beasiswa</h1>
-      <ScholarshipForm />
+
+      <Form>
+
+        <FormGroup>
+          <Label for="title">Judul</Label>
+          <Input id="title" name="title" type="text" onChange={(e) => setTitle(e.target.value)} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="poster">Poster</Label>
+          {image && <img className="preview d-block mb-3" src={image} alt="preview" />}
+          <Input id="poster" name="poster" type="file" onChange={(e) => onPosterUpload(e)} />
+          <FormText>&#8505; Menaambahkan poster dapat membuat mahasiswa lebih tertarik dan informasi mudah dipahami.</FormText>
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="deadline">Batas Pengajuan</Label>
+          <Input id="deadline" name="deadline" type="date" onChange={(e) => setDeadline(e.target.value)} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="description">Deskripsi</Label>
+          <textarea className="form-control" name="description" id="description" rows="10" onChange={(e) => setDescription(e.target.value)}></textarea>
+          <FormText>&#8505; Jelaskan detail beasiswa yang berisi cakupan, syarat, dan cara mendaftar beasiswa.</FormText>
+        </FormGroup>
+
+        <div className="d-flex justify-content-end">          
+          <Button color="primary" className="px-4" onClick={onSubmit}>Tambah</Button>
+        </div>
+
+      </Form>
+
     </main>
   )
 }
--- src/pages/Scholarship/index.js
+++ src/pages/Scholarship/index.js
@@ -4,14 +4,31 @@ import { ScholarshipCard } from '../../components'
 
 const Scholarship = () => {
   const [dataScholarship, setDataScholarship] = useState([]);
+  const [pageInformation, setPageInformation] = useState({})
+  const [counter, setCounter] = useState(1);
+
+  const {total_data, per_page, current_page} = pageInformation;
+  const total_page = Math.ceil(total_data/per_page);
+
+  const previous = () => {
+    setCounter(counter <= 1 ? 1 : (counter - 1));
+  }
+
+  const next = () => {
+    setCounter(counter >= total_page ? total_page : (counter + 1));
+  }
+
   useEffect(() => {
-    fetch('http://localhost:4000/v1/scholarships')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setDataScholarship(responseJson.data);
-      })
-      .catch((err) => console.log(err));
-  });
+    const fetchData = async () => {
+      let response = await fetch(`http://localhost:4000/v1/scholarships?page=${counter}&perPage=3`);
+      response = await response.json();
+      setDataScholarship(response.data);
+      setPageInformation(response);
+    }
+    
+    fetchData();
+  }, [counter]);
+  
   return (
     <main className="container my-5">
       <div className="d-flex justify-content-end mb-4">
@@ -23,7 +40,7 @@ const Scholarship = () => {
         {dataScholarship.map((scholarship) => {
           return (
             <div className="col-md-4">
-              <ScholarshipCard 
+              <ScholarshipCard
                 key={scholarship._id}
                 title={scholarship.title}
                 deadline={scholarship.deadline}
@@ -36,8 +53,9 @@ const Scholarship = () => {
         })}
       </div>
       <div className="d-flex justify-content-center">
-        <Button className="mx-2" color="warning">&#10094;</Button>
-        <Button className="mx-2" color="warning">&#10095;</Button>
+        <Button className="mx-2" color="warning" onClick={previous}>&#10094;</Button>
+        <p className="d-flex my-0 mx-4 align-items-center fw-bold">{current_page} / {total_page}</p>
+        <Button className="mx-2" color="warning" onClick={next}>&#10095;</Button>
       </div>
     </main>
   )
